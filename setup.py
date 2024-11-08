ffa8410e610633e5f03a28f1e4082f9b42857d91
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2024-11-08 12:35:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-from FACTMx import __version__
+#from FACTMx import __version__
 
 setup(
     name='FACTMx',
-    version=__version__,
+    version='dev',
 
     url='https://github.com/KazikOrzechowski/FACTMx',
     author='Kazimierz Oksza-Orzechowski',
