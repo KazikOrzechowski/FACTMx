8e8f77a6d5900cd6dff392f08de285f6934bf504
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2024-11-08 12:06:58 +0100

    Update FACTMx_model.py

--- FACTMx/FACTMx_model.py
+++ FACTMx/FACTMx_model.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 from typing import Tuple, Dict
-from . import FACTMx_head
-from . import FACTMx_encoder
+from FACTMx import FACTMx_head
+from FACTMx import FACTMx_encoder
 
 from logging import warning
 import json
