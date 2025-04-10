ffed5dfa408e6bafdb5a645765e6e013b3e2bff6
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-04-10 17:21:22 +0200

    Update FACTMx_encoder.py

--- FACTMx/FACTMx_encoder.py
+++ FACTMx/FACTMx_encoder.py
@@ -3,6 +3,14 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 
+try:
+  from tensorflow_model_optimization.python.core.keras.compat import keras
+  _TFMOT_IS_LOADED = True
+except ImportError:
+  warning('TensorFlow Resources Model optimization module not found, weight pruning is disabled.')
+  import tensorflow.keras as keras
+  _TFMOT_IS_LOADED = False
+
 from typing import Tuple, Dict
 
 from FACTMx.custom_keras_layers import ConstantResponse
@@ -14,9 +22,12 @@ class FACTMx_encoder(tf.Module):
 
   def __init__(self, dim_latent, head_dims,
                layer_configs={'loc':'linear', 'scale':'linear'},
-               name=None, prior_params=None, eps=1E-5):
+               name=None, 
+               prior_params=None, 
+               eps=1E-5,
+               pruning_params={'loc':None, 'scale':None}):
     super().__init__(name=name)
-
+    self.pruning_params = pruning_params
     self.dim_latent = dim_latent
     self.head_dims = head_dims
     self.eps = eps
@@ -24,13 +35,17 @@ class FACTMx_encoder(tf.Module):
 
     loc_config = layer_configs.pop('loc', 'linear')
     if loc_config == 'linear':
-      self.layers['loc'] = tf.keras.Sequential(
+      self.layers['loc'] = keras.Sequential(
                               [tf.keras.Input(shape=(sum(head_dims),)),
                                tf.keras.layers.Dense(units=dim_latent,
                                                      kernel_initializer='orthogonal')]
       )
     else:
-      self.layers['loc'] = tf.keras.Sequential.from_config(loc_config)
+      self.layers['loc'] = keras.Sequential.from_config(loc_config)
+
+    loc_pruning = pruning_params.pop('loc', None)
+    if loc_pruning is not None and _TFMOT_IS_LOADED:
+      self.layers['loc'] = tfmot.sparsity.keras.prune_low_magnitude(self.layers['loc'], **loc_pruning)
 
     scale_config = layer_configs.pop('scale', 'linear')
     if scale_config == 'linear':
@@ -42,6 +57,10 @@ class FACTMx_encoder(tf.Module):
       )
     else:
       self.layers['scale'] = tf.keras.Sequential.from_config(scale_config)
+
+    scale_pruning = pruning_params.pop('scale', None)
+    if scale_pruning is not None and _TFMOT_IS_LOADED:
+      self.layers['scale'] = tfmot.sparsity.keras.prune_low_magnitude(self.layers['scale'], **scale_pruning)
 
     self.t_vars = tuple(var for layer in self.layers.values() for var in layer.trainable_variables)
 
@@ -98,6 +117,7 @@ class FACTMx_encoder(tf.Module):
                 "eps": self.eps,
                 "prior_params": {'loc':self.prior.loc.numpy().tolist(),
                                  'scale_tril':self.prior.scale_tril.numpy().tolist()},
+                "pruning_params": self.pruning_params,
                 "layer_configs": {key: layer.get_config() for key, layer in self.layers.items()}
              }
 
