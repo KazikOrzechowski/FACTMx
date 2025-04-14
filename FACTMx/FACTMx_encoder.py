d3d6d8669c6958cb34a3e72a973ac38d0644b4e9
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-04-14 11:39:28 +0200

    Update FACTMx_encoder.py

--- FACTMx/FACTMx_encoder.py
+++ FACTMx/FACTMx_encoder.py
@@ -24,10 +24,8 @@ class FACTMx_encoder(tf.Module):
                layer_configs={'loc':'linear', 'scale':'linear'},
                name=None, 
                prior_params=None, 
-               eps=1E-5,
-               pruning_params={'loc':None, 'scale':None}):
+               eps=1E-5,):
     super().__init__(name=name)
-    self.pruning_params = pruning_params
     self.dim_latent = dim_latent
     self.head_dims = head_dims
     self.eps = eps
@@ -43,11 +41,6 @@ class FACTMx_encoder(tf.Module):
     else:
       self.layers['loc'] = keras.Sequential.from_config(loc_config)
 
-    loc_pruning = pruning_params.pop('loc', None)
-    if loc_pruning is not None and _TFMOT_IS_LOADED:
-      self.layers['loc'] = tfmot.sparsity.keras.prune_low_magnitude(self.layers['loc'],
-                                                                    pruning_schedule=tfmot.sparsity.keras.PolynomialDecay.from_config(loc_pruning))
-
     scale_config = layer_configs.pop('scale', 'linear')
     if scale_config == 'linear':
       self.layers['scale'] = tf.keras.Sequential(
@@ -58,11 +51,6 @@ class FACTMx_encoder(tf.Module):
       )
     else:
       self.layers['scale'] = tf.keras.Sequential.from_config(scale_config)
-
-    scale_pruning = pruning_params.pop('scale', None)
-    if scale_pruning is not None and _TFMOT_IS_LOADED:
-      self.layers['scale'] = tfmot.sparsity.keras.prune_low_magnitude(self.layers['scale'],
-                                                                      pruning_schedule=tfmot.sparsity.keras.PolynomialDecay.from_config(scale_pruning))
 
     self.t_vars = tuple(var for layer in self.layers.values() for var in layer.trainable_variables)
 
@@ -119,7 +107,6 @@ class FACTMx_encoder(tf.Module):
                 "eps": self.eps,
                 "prior_params": {'loc':self.prior.loc.numpy().tolist(),
                                  'scale_tril':self.prior.scale_tril.numpy().tolist()},
-                "pruning_params": self.pruning_params,
                 "layer_configs": {key: layer.get_config() for key, layer in self.layers.items()}
              }
 
@@ -127,14 +114,6 @@ class FACTMx_encoder(tf.Module):
 
   def from_config(config):
     return FACTMx_encoder(**config)
-
-  def save_weights(self, encoder_path):
-    for key, layer in self.layers.items():
-      layer.save_weights(f'{encoder_path}_{key}.weights.h5')
-
-  def load_weights(self, encoder_path):
-    for key, layer in self.layers.items():
-      layer.load_weights(f'{encoder_path}_{key}.weights.h5')
 
   def save_weights(self, encoder_path):
     for key, layer in self.layers.items():
