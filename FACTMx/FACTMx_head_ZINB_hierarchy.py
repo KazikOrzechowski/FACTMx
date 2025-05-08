04eb542f42c57e1cac298b454cd3159ad6d04d86
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-05-08 11:46:30 +0200

    Update FACTMx_head_ZINB_hierarchy.py

--- FACTMx/FACTMx_head_ZINB_hierarchy.py
+++ FACTMx/FACTMx_head_ZINB_hierarchy.py
@@ -215,7 +215,7 @@ class FACTMx_head_ZINB_hierarchy(FACTMx_head):
         'temperature':self.temperature,
         'prop_loss_scale':self.prop_loss_scale,
         "layer_configs": {key: layer.get_config() for key, layer in self.layers.items()},
-        'mixture_params':[
+        'mixture_params_list':[
           {
           'logits':logits.numpy().tolist(),
           'log_total_count':log_total_count.numpy().tolist(),
