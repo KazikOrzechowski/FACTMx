cc63e1d0d992772d4c6456cc05ae69f73cda757b
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-07-06 14:31:24 +0200

    Update FACTMx_head.py

--- FACTMx/FACTMx_head.py
+++ FACTMx/FACTMx_head.py
@@ -133,71 +133,99 @@ class FACTMx_head_Multinomial(FACTMx_head):
   head_type = 'Multinomial'
 
   def __init__(self,
+               dim_pos,
+               dim_cat,
                dim, dim_latent, head_name,
                layer_configs={'logits':'linear'},
-               eps = 1E-3, 
+               eps = 1E-3,
                **kwargs):
+    #dim is the dimension of head's output to encoder
     super().__init__(dim, dim_latent, head_name)
     self.eps = eps
+    self.dim_pos = dim_pos
+    self.dim_cat = dim_cat
+    _dim_logits = dim_pos * (dim_cat-1)
 
-    logits_config = layer_configs.pop('logits', 'linear')             
+    logits_config = layer_configs.pop('logits', 'linear')
     if logits_config == 'linear':
       self.layers['logits'] = tf.keras.Sequential(
-                            [tf.keras.Input(shape=(self.dim_latent,)),
-                             tf.keras.layers.Dense(self.dim)]
-                          )
+                              [tf.keras.Input(shape=(self.dim_latent,)),
+                              tf.keras.layers.Dense(_dim_logits)]
+                      )
     else:
       self.layers['logits'] = tf.keras.Sequential.from_config(logits_config)
 
-    assert self.layers['logits'].output_shape == (None, self.dim)
+    assert self.layers['logits'].output_shape == (None, _dim_logits)
     assert self.layers['logits'].input_shape == (None, self.dim_latent)
 
     self.t_vars = self.layers['logits'].trainable_variables
 
+    preencoder_config = layer_configs.pop('preencoder', None)
+    if preencoder_config is None:
+      self.preencoder = False
+    else:
+      self.preencoder = True
+      self.layers['preencoder'] = tf.keras.Sequential.from_config(preencoder_config)
+      self.t_vars += self.layers['preencoder'].trainable_variables
+
+      assert self.layers['preencoder'].output_shape == (None, self.dim)
+      assert self.layers['preencoder'].input_shape == (None, dim_pos * dim_cat)
+
+
   def decode_params(self, latent):
     #decode logits from a latent point
-    return self.layers['logits'](latent)
+    return tf.reshape(self.layers['logits'](latent), shape=(-1, self.dim_pos, self.dim_cat-1))
 
   def make_decoder(self, latent, counts):
     #return the decoding distribution given its latent point
     logits = self.decode_params(latent)
     padded_logits = tf.pad(logits,
-                           tf.constant([[0, 0], [1, 0]]),
-                           'CONSTANT')
+                        tf.constant([[0, 0], [0, 0], [1, 0]]),
+                        'CONSTANT')
     return tfp.distributions.Multinomial(total_count=counts, logits=padded_logits)
 
   def decode(self, latent, data):
     #decode a sample from latent
-    counts = data[1]
+    observations, counts = data
     return self.make_decoder(latent, counts).sample()
 
   def encode(self, data):
-    #give logits to encode
-    logits = tf.math.log(data[0] + self.eps)
-    normalized = logits[:,1:] - tf.reshape(logits[:,0], shape=(-1,1))
-    return {'encoder_input': normalized}
+    #give logits or preencoding to encode
+    observations, counts = data
+    if self.preencoder:
+      preencoder_input = tf.reshape(observations, shape=(-1, self.dim_pos * self.dim_cat))
+      encoder_input = self.layers['preencoder'](preencoder_input)
+      return {'encoder_input': encoder_input}
+    else:
+      logits = tf.math.log(counts + self.eps)
+      normalized = logits[:,1:] - tf.reshape(logits[:,0], shape=(-1,1))
+      return {'encoder_input': normalized}
 
   def loss(self, data, latent, beta=1):
     #return -loglikelihood of data given its latent point
     observations, counts = data
     log_prob = self.make_decoder(latent, counts).log_prob(observations)
 
-    loss = -tf.reduce_mean(log_prob)
+    loss = -tf.reduce_sum(log_prob) / observations.shape[0]
     loss += tf.reduce_sum(self.layers['logits'].losses)
 
-    return loss 
+    return loss
 
   def get_config(self):
-    config = super().get_config()
-    config.update({
+    config = {
         'head_type': self.head_type,
-        'layer_configs': {'logits': self.layers['logits'].get_config()}
-    })
+        'dim_pos': self.dim_pos,
+        'dim_cat': self.dim_cat,
+        'dim': self.dim,
+        'dim_latent': self.dim_latent,
+        'head_name': self.head_name,
+        'layer_configs': {key: layer.get_config() for key, layer in self.layers.items()},
+        'eps': self.eps,
+    }
     return config
 
   def from_config(config):
     return FACTMx_head_Multinomial(**config)
-
 
 
 class FACTMx_head_MultiNormal(FACTMx_head):
