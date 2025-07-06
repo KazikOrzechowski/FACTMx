1ba79eee8956bb6f490d1cc88acf6466b985367d
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-07-06 14:31:55 +0200

    Update FACTMx_head_ClonalTree.py

--- FACTMx/FACTMx_head_ClonalTree.py
+++ FACTMx/FACTMx_head_ClonalTree.py
@@ -20,7 +20,7 @@ class FACTMx_head_ClonalTree(FACTMx_head):
                dim, dim_latent, dim_pos, dim_clones, levels,
                head_name,
                log_mut_assignment=None,
-               layer_configs={'log_probs':'linear', 'encoder_classifier':'linear'},
+               layer_configs={'logits':'linear', 'encoder_classifier':'linear'},
                prob_obs=.5, prob_unobs=.01,
                temperature=1E-4, eps=1E-3,
                prop_loss_scale=1.):
@@ -38,18 +38,18 @@ class FACTMx_head_ClonalTree(FACTMx_head):
     # >>> initialise layers >>>
     assert self.dim == self.dim_clones + 1 #we classify among tumour clones and one reference clone
 
-    mixture_logits_config = layer_configs.pop('log_probs', 'linear')
+    mixture_logits_config = layer_configs.pop('logits', 'linear')
     if mixture_logits_config == 'linear':
-      self.layers['log_probs'] = tf.keras.Sequential(
+      self.layers['logits'] = tf.keras.Sequential(
                                         [tf.keras.Input(shape=(self.dim_latent,)),
                                          tf.keras.layers.Dense(units=self.dim,
                                                                kernel_initializer='orthogonal')]
                                       )
     else:
-      self.layers['log_probs'] = tf.keras.Sequential.from_config(mixture_logits_config)
+      self.layers['logits'] = tf.keras.Sequential.from_config(mixture_logits_config)
 
-    assert self.layers['log_probs'].output_shape == (None, self.dim)
-    assert self.layers['log_probs'].input_shape == (None, self.dim_latent)
+    assert self.layers['logits'].output_shape == (None, self.dim)
+    assert self.layers['logits'].input_shape == (None, self.dim_latent)
 
     encoder_classifier_config = layer_configs.pop('encoder_classifier', 'linear')
     if encoder_classifier_config == 'linear':
@@ -68,24 +68,29 @@ class FACTMx_head_ClonalTree(FACTMx_head):
     # >>> initialise clones >>>
     self.all_tumour_clones = int(2 ** (self.levels+1) - 1)
 
+    if log_mut_assignment is None:
+      self.log_mut_assignment = tf.keras.Variable(tf.zeros((self.dim_pos, self.all_tumour_clones)),
+                                                  trainable=True,
+                                                  dtype=tf.float32)
+    else:
+      self.log_mut_assignment = tf.keras.Variable(log_mut_assignment,
+                                                  trainable=True,
+                                                  dtype=tf.float32)
 
-    self.log_mut_assignment = tf.Variable(tf.zeros((self.dim_pos, self.all_tumour_clones)),
-                                           trainable=True,
-                                           dtype=tf.float32)
     self.level_shapes = [(self.dim_pos,) + (2,) * i + (1,) * (self.levels-i) for i in range(self.levels+1)]
     self.level_inds = [slice(2**i - 1, 2**(i+1)-1) for i in range(self.levels+1)]
     # <<< initialise clones <<<
 
     # get training variables
-    self.t_vars = [*self.layers['mixture_logits'].trainable_variables,
+    self.t_vars = [*self.layers['logits'].trainable_variables,
                    *self.layers['encoder_classifier'].trainable_variables,
-                   *self.log_mut_assignment]
+                   self.log_mut_assignment]
 
   def get_assignment_distribution(self, logits):
     return tfp.distributions.RelaxedOneHotCategorical(logits=logits, temperature=self.temperature)
 
   def decode_log_probs(self, latent):
-    log_probs = self.layers['log_probs'](latent)
+    log_probs = self.layers['logits'](latent)
 
     # minimum topic proportion is EPS
     #log_eps = tf.constant(tf.math.log(self.eps), shape=log_mixture_probs.shape)
@@ -93,28 +98,33 @@ class FACTMx_head_ClonalTree(FACTMx_head):
 
   def get_clone_profiles_sample(self):
     mut_assignment_sample = self.get_assignment_distribution(self.log_mut_assignment).sample()
-    all_levels = [tf.reshape(mut_assignment_sample[:,inds], shape) for shape, inds in zip(self.level_shape, self.level_inds)]
-    
-    clonal_profiles = tf.reduce_sum(tf.stack(all_levels), axis=0)
+    all_levels = [tf.reshape(mut_assignment_sample[:,inds], shape) for shape, inds in zip(self.level_shapes, self.level_inds)]
+    all_levels = [tf.broadcast_to(level, self.level_shapes[-1]) for level in all_levels]
+
+    clonal_profiles = tf.reduce_sum(all_levels, axis=0)
     clonal_profiles = tf.reshape(clonal_profiles, (self.dim_pos, self.dim_clones))
-    clonal_profiles = tf.concat([tf.zeros((self.dim_pos,1)), clonal_profiles]) #add reference clone
+    clonal_profiles = tf.concat([tf.zeros((self.dim_pos,1)), clonal_profiles], axis=1) #add reference clone
     clonal_profiles = tf.transpose(clonal_profiles)
 
     return clonal_profiles
 
   def get_clone_distributions(self, clonal_profiles, counts):
     probs = clonal_profiles * self.prob_obs + (1-clonal_profiles) * self.prob_unobs
+    probs = tf.expand_dims(probs, axis=0)
     return tfp.distributions.Binomial(total_count=counts, probs=probs)
 
   def decode(self, latent, data):
-    counts, mut = data
+    mut, counts = data
+    mut = tf.expand_dims(mut, axis=1)
+    counts = tf.expand_dims(counts, axis=1)
 
-    log_probs = self.decode_log_probs(latent)
-    logits = log_probs + self.get_clone_distributions(self.get_clone_profiles_sample(), counts).log_prob(mut)
+    log_prior = self.decode_log_probs(latent)
+    log_like = self.get_clone_distributions(self.get_clone_profiles_sample(), counts).log_prob(mut)
+    log_post = log_prior + tf.reduce_sum(log_like, axis=-1)
 
-    sample = self.get_assignment_distribution(logits).sample()
+    sample = self.get_assignment_distribution(log_post).sample()
 
-    return sample, logits, log_probs
+    return sample, log_post, log_prior
 
   def loss(self,
             data,
@@ -128,11 +138,13 @@ class FACTMx_head_ClonalTree(FACTMx_head):
     log_loss = log_loss / _batch_size
 
     return tf.reduce_sum([log_loss,
-                          *self.layers['mixture_logits'].losses,
+                          *self.layers['logits'].losses,
                           *self.layers['encoder_classifier'].losses])
 
   def encode(self, data):
-    assignment_logits = self.layers['encoder_classifier'](data)
+    mut, counts = data
+
+    assignment_logits = self.layers['encoder_classifier'](mut)
     #assignment_logits = tf.math.log(tf.math.exp(assignment_logits) + self.eps)
     assignment_sample = self.get_assignment_distribution(assignment_logits).sample()
 
