0cd3047ebe07a785ff18af00194be1d6331b22de
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-08-11 14:23:21 +0200

    Update FACTMx_head_GMM_hierarchy.py

--- FACTMx/FACTMx_head_GMM_hierarchy.py
+++ FACTMx/FACTMx_head_GMM_hierarchy.py
@@ -75,8 +75,8 @@ class FACTMx_head_GMM_hierarchy(FACTMx_head):
 
     for level, level_params in enumerate(mixture_params_list):
       _level_shape = (dim_topics,) * (level+1) + (dim_normal,)
-      _loc_init = tf.keras.initializers.RandomNormal(stddev=dim_topics ** (-3*level))
-      _log_scale_init = tf.ones(_level_shape) * (-level-3)
+      _loc_init = tf.keras.initializers.RandomNormal(stddev=dim_topics ** (-2*level))
+      _log_scale_init = tf.ones(_level_shape) * (-level-2)
 
       if 'loc' in level_params.keys():
         loc = level_params['loc']
