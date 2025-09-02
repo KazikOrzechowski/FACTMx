d9cfb4a277767aa133b748c5a31770df16c72fb9
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-09-02 20:45:12 +0200

    Update FACTMx_head_ZINB_mixture3.py

--- FACTMx/FACTMx_head_ZINB_mixture3.py
+++ FACTMx/FACTMx_head_ZINB_mixture3.py
@@ -2,14 +2,6 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import numpy as np
 
-try:
-  import tensorflow_model_optimization as tfmot
-  from tensorflow_model_optimization.python.core.keras.compat import keras
-  _TFMOT_IS_LOADED = True
-except ImportError:
-  import tensorflow.keras as keras
-  _TFMOT_IS_LOADED = False
-
 from FACTMx.FACTMx_head import FACTMx_head
 
 
@@ -20,18 +12,20 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
                dim, dim_latent, dim_counts,
                head_name,
                layer_configs={'mixture_logits':'linear', 'encoder_classifier':'linear'},
-               mixture_params={'logits':None, 'log_count_spread':None, 'inflated_loc_logits':None},
+               mixture_params={'logits':None, 'log_total_count':None, 'inflated_loc_logits':None},
                marker_groups=None,
                temperature=1E-4,
                eps=1E-3,
-               prop_loss_scale=1.):
+               prop_loss_scale=1., marker_loss_scale=0., entropy_loss_scale=0.,
+              ):
     super().__init__(dim, dim_latent, head_name)
 
-    self.dim_mixtures = dim + 1
     self.dim_counts = dim_counts
     self.temperature = temperature
     self.eps = eps
     self.prop_loss_scale = prop_loss_scale
+    self.marker_loss_scale = marker_loss_scale
+    self.entropy_loss_scale = entropy_loss_scale
     self.marker_groups = marker_groups
 
     # >>> initialise layers >>>
@@ -52,30 +46,30 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
     if encoder_classifier_config == 'linear':
       self.layers['encoder_classifier'] = tf.keras.Sequential(
                                             [tf.keras.Input(shape=(None, self.dim_counts)),
-                                             tf.keras.layers.Dense(units=self.dim_mixtures,
+                                             tf.keras.layers.Dense(units=self.dim,
                                                                    activation='log_softmax')]
                                           )
     else:
       self.layers['encoder_classifier'] = tf.keras.Sequential.from_config(encoder_classifier_config)
 
     assert self.layers['encoder_classifier'].input_shape == (None, None, self.dim_counts)
-    assert self.layers['encoder_classifier'].output_shape == (None, None, self.dim_mixtures)
+    assert self.layers['encoder_classifier'].output_shape == (None, None, self.dim)
     # <<< initialise layers <<<
 
     # >>> initialise mixtures >>>
     logits = mixture_params.pop('logits', None)
     if logits is None:
-      logits = tf.keras.initializers.RandomNormal()(shape=(self.dim_mixtures, self.dim_counts))
+      logits = tf.keras.initializers.RandomNormal()(shape=(self.dim, self.dim_counts))
 
     self.logits = tf.keras.Variable(logits,
                                     trainable=True,
                                     dtype=tf.float32)
 
-    log_count_spread = mixture_params.pop('log_count_spread', None)
-    if log_count_spread is None:
-      log_count_spread = tf.keras.initializers.Zeros()(shape=(self.dim_mixtures, self.dim_counts))
+    log_total_count = mixture_params.pop('log_total_count', None)
+    if log_total_count is None:
+      log_total_count = tf.keras.initializers.Zeros()(shape=(1, self.dim_counts))
 
-    self.log_count_spread = tf.keras.Variable(log_count_spread,
+    self.log_total_count = tf.keras.Variable(log_total_count,
                                               trainable=True,
                                               dtype=tf.float32)
 
@@ -92,7 +86,7 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
     self.t_vars = [*self.layers['mixture_logits'].trainable_variables,
                    *self.layers['encoder_classifier'].trainable_variables,
                    self.logits,
-                   self.log_count_spread,
+                   self.log_total_count,
                    self.inflated_loc_logits]
 
 
@@ -101,21 +95,15 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
 
 
   def get_mixture_distributions(self, library_sizes):
-    _broad_mix_shape = (1, 1, self.dim_mixtures, self.dim_counts)
-    _broad_data_shape = library_sizes.shape + (1, 1)
-    _broad_inflation_shape = library_sizes.shape + (self.dim_mixtures, self.dim_counts)
+    _mix_shape = (self.dim, self.dim_counts)
     
-    inflated_loc_probs = tf.math.sigmoid(self.inflated_loc_logits) * .9
-    count_spread = tf.nn.softmax(self.log_count_spread, axis=1)
+    inflated_loc_probs = tf.math.sigmoid(self.inflated_loc_logits) * .75
+    total_count = tf.math.exp(self.log_total_count) + self.eps
 
-    logits = tf.reshape(self.logits, _broad_mix_shape)
-    total_count = tf.reshape(library_sizes, _broad_data_shape) * tf.reshape(count_spread, _broad_mix_shape) + self.eps
-    inflated_loc_probs = tf.broadcast_to(inflated_loc_probs, _broad_inflation_shape)
-    
     return tfp.distributions.ZeroInflatedNegativeBinomial(
-        logits=logits,
-        total_count=total_count,
-        inflated_loc_probs=inflated_loc_probs,
+        logits=self.logits,
+        total_count=tf.broadcast_to(total_count, _mix_shape),
+        inflated_loc_probs=tf.broadcast_to(inflated_loc_probs, _mix_shape),
         require_integer_total_count=False,
     )
 
@@ -125,11 +113,7 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
 
 
   def decode_log_mixture_probs(self, latent):
-    paddings_probs = tf.constant([[0, 0], [1, 0]])
-    log_mixture_probs = tf.pad(self.layers['mixture_logits'](latent),
-                                paddings_probs,
-                                'CONSTANT')
-    log_mixture_probs = tf.math.log(tf.nn.softmax(log_mixture_probs, axis=-1))
+    log_mixture_probs = self.layers['mixture_logits'](latent)
 
     # minimum topic proportion is EPS
     log_eps = tf.constant(tf.math.log(self.eps), shape=log_mixture_probs.shape)
@@ -137,15 +121,13 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
 
 
   def decode(self, latent, data, sample=False):
-    counts, library_sizes = data
-    
     log_mixture_probs = self.decode_log_mixture_probs(latent)
     log_mixture_probs = tf.expand_dims(log_mixture_probs, 1) #(n_batch, 1, n_mix)
 
-    mixtures = self.get_mixture_distributions(library_sizes)
+    mixtures = self.get_mixture_distributions()
 
     _broad_counts = tf.expand_dims(counts, -2) #(n_batch, n_subbatch, 1, n_counts)
-    log_likelihoods = mixtures.log_prob(_broad_counts) 
+    log_likelihoods = mixtures.log_prob(data) 
     log_likelihoods = tf.reduce_sum(log_likelihoods, axis=-1)
 
     assignment_sample = self.get_assignment_distribution(log_mixture_probs + log_likelihoods).sample() if sample else None
@@ -176,31 +158,37 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
 
     marker_loss = tf.constant(0.)
     if self.marker_groups is not None:
-      probs = tf.math.sigmoid(self.logits)
+      probs = tf.math.softmax(encoder_assignment_logits, axis=-1)
       
-      markers = [tf.reduce_mean(tf.gather(probs, marker_inds, axis=-1), axis=-1) for marker_inds, _ in self.marker_groups]
-      antagonists = [tf.reduce_mean(tf.gather(probs, antagonist_inds, axis=-1), axis=-1) for _, antagonist_inds in self.marker_groups]
+      data = tf.expand_dims(data, axis=-2)
+      markers = [tf.reduce_sum(tf.gather(data, marker_inds, axis=-1), axis=-1) for marker_inds, _ in self.marker_groups]
+      antagonists = [tf.reduce_sum(tf.gather(data, antagonist_inds, axis=-1), axis=-1) for _, antagonist_inds in self.marker_groups]
 
-      marker_loss = tf.reduce_mean(tf.stack(markers) * tf.stack(antagonists))
+      marker_loss = tf.reduce_mean(tf.stack(markers) * tf.stack(antagonists) * tf.expand_dims(probs, axis=0))
+
+    entropy_loss = tf.constant(0.)
+    if self.entropy_loss_scale != 0:
+      entropy = tf.math.softmax(encoder_assignment_logits, axis=-1)
+      entropy = tf.reduce_sum(entropy, axis=0)
+      entropy = entropy * tf.math.log(entropy)
+      entropy_loss = -tf.reduce_sum(entropy)
 
     return tf.reduce_sum([self.prop_loss_scale*kl_loss,
                           ll_loss,
-                          marker_loss,
+                          marker_loss * self.marker_loss_scale,
+                          entropy_loss * self.entropy_loss_scale,
                           *self.layers['mixture_logits'].losses,
                           *self.layers['encoder_classifier'].losses])
 
 
   def encode(self, data):
-    counts, library_sizes = data
-    assignment_logits = self.layers['encoder_classifier'](counts)
+    assignment_logits = self.layers['encoder_classifier'](data)
     assignment_sample = self.get_assignment_distribution(assignment_logits).sample()
 
     proportions_sample = tf.reduce_mean(assignment_sample, axis=1) + self.eps
     log_proportions_sample = tf.math.log(proportions_sample)
 
-    encoder_input = log_proportions_sample[:,1:] - tf.reshape(log_proportions_sample[:,0], (-1, 1))
-
-    return {'encoder_input': encoder_input,
+    return {'encoder_input': log_proportions_sample,
             'encoder_assignment_sample': assignment_sample,
             'encoder_assignment_logits': assignment_logits}
 
@@ -215,10 +203,12 @@ class FACTMx_head_ZINB_mixture3(FACTMx_head):
         'marker_groups':self.marker_groups,
         'temperature':self.temperature,
         'prop_loss_scale':self.prop_loss_scale,
+        'marker_loss_scale':self.marker_loss_scale,
+        'entropy_loss_scale':self.entropy_loss_scale,
         "layer_configs": {key: layer.get_config() for key, layer in self.layers.items()},
         'mixture_params':{
             'logits':self.logits.numpy().tolist(),
-            'log_count_spread':self.log_count_spread.numpy().tolist(),
+            'log_total_count':self.log_total_count.numpy().tolist(),
             'inflated_loc_logits':self.inflated_loc_logits.numpy().tolist(),
         },
     }
