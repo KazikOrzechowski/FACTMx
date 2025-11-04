c4151b70db5319d82acffacbb1196672492fab4a
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-11-04 11:04:24 +0100

    Update FACTMx_head.py

--- FACTMx/FACTMx_head.py
+++ FACTMx/FACTMx_head.py
@@ -253,8 +253,8 @@ class FACTMx_head_MultiNormal(FACTMx_head):
                               [tf.keras.Input(shape=(self.dim_latent,)),
                                ConstantResponse(units=self.dim,
                                                 activation='sigmoid',
-                                                bias_initializer={'class_name':'Constant', 'config':{'value':np.log(eps)}})],
-                               tf.keras.layers.Rescaling(scale=.5))
+                                                bias_initializer={'class_name':'Constant', 'config':{'value':np.log(eps)}}),
+                               tf.keras.layers.Rescaling(scale=.5)])
     else:
       self.layers['scale'] = tf.keras.Sequential.from_config(scale_config)
 
