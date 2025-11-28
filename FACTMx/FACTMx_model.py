5b7b4462346a8592dba8255a295b317ce6796eee
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2025-11-28 17:25:06 +0100

    Update FACTMx_model.py

--- FACTMx/FACTMx_model.py
+++ FACTMx/FACTMx_model.py
@@ -75,8 +75,8 @@ class FACTMx_model(tf.Module):
 
   def elbo(self, data):
     if self.multi:
+      parallel_encode = lambda head, head_data: get_attr(head, 'encode')(head_data)
       with multiprocessing.Pool(processes=len(self.heads)) as pool:
-        parallel_encode = lambda head, head_data: get_attr(head, 'encode')(head_data)
         head_kwargs = pool.starmap(parallel_encode, zip(self.heads, data))
     else:
       head_kwargs = [head.encode(data[i]) for i, head in enumerate(self.heads)]
@@ -85,8 +85,8 @@ class FACTMx_model(tf.Module):
     latent, kl_loss = self.encoder.encode_with_loss(tf.concat(head_encoded, axis=-1))
 
     if self.multi:
+      parallel_loss = lambda head, head_data, kwargs: get_attr(head, 'loss')(head_data, latent, beta=self.beta, **kwargs)
       with multiprocessing.Pool(processes=len(self.heads)) as pool:
-        parallel_loss = lambda head, head_data, kwargs: get_attr(head, 'loss')(head_data, latent, beta=self.beta, **kwargs)
         decoding_losses = pool.starmap(parallel_loss, zip(self.heads, data, head_kwargs))
     else:
       decoding_losses = [head.loss(data[i],
