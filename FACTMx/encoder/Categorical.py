142d79882ffea19e351445b04c033e6fc0800abe
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2026-06-15 22:52:51 +0200

    Update Categorical.py

--- FACTMx/encoder/Categorical.py
+++ FACTMx/encoder/Categorical.py
@@ -94,7 +94,7 @@ class Categorical(FACTMx_encoder):
     if prior_params is None:
       # A vector prior is the natural unbatched prior for a Dirichlet posterior
       # with event shape ``dim_latent``. 
-      concentration = tf.ones((dim_latent,), dtype=tf.float32) / 10.
+      concentration = tf.ones((dim_latent,), dtype=tf.float32)
       self.prior = tfp.distributions.Dirichlet(concentration)
     else:
       self.prior = tfp.distributions.Dirichlet(**prior_params)
