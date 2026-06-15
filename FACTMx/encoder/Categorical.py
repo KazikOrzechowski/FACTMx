6f9008da8d009f2b0c2b3726527ee98e8e0c0df0
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2026-06-15 22:50:50 +0200

    Update Categorical.py

--- FACTMx/encoder/Categorical.py
+++ FACTMx/encoder/Categorical.py
@@ -124,8 +124,8 @@ class Categorical(FACTMx_encoder):
     """Return a latent sample together with the mean KL-to-prior loss."""
     encoder = self.make_encoder(data)
     sample = encoder.sample()
-    # sample, _ = self.encode_params(data)
-    loss = tf.reduce_mean(encoder.kl_divergence(self.prior)) * 0
+    sample, _ = self.encode_params(data)
+    loss = tf.reduce_mean(encoder.kl_divergence(self.prior))
     for layer in self.layers.values():
       loss += tf.reduce_sum(layer.losses)
     return sample, loss
