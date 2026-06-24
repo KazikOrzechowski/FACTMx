d229b232594417ba2e3bb2d2a51554a2d1c38cda
KazikOrzechowski <94362388+KazikOrzechowski@users.noreply.github.com>
2026-06-24 19:37:46 +0200

    Update CategoricalContrastiveLoss.py

--- FACTMx/encoder/CategoricalContrastiveLoss.py
+++ FACTMx/encoder/CategoricalContrastiveLoss.py
@@ -81,7 +81,9 @@ class CategoricalContrastiveLoss(FACTMx_encoder):
     if self.max_pairs is None:
       return pairs
     inds = []
+    print(tf.unique(pairs[:,0])[0])
     for i in tf.unique(pairs[:,0])[0]:
+      print(i)
       inds.extend(tf.where(pairs[:,0] == i)[-self.max_pairs:])
     inds = tf.stack(inds)
     return tf.gather(pairs, inds, axis=0)
